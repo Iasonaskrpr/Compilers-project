d463be97ad9446b74afbfdaa13e591d6a84bc2f2
Iasonaskrpr <iaskarapro@gmail.com>
2025-04-03 19:16:32 +0300

    Parser for ir done some testing still needs to be done

--- Part2/Main.java
+++ Part2/Main.java
@@ -3,21 +3,15 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
+        PrintStream outFile = new PrintStream(new FileOutputStream("Translated.ir"));
+        System.setOut(outFile);
         // Read input from the file
         FileReader fileReader = new FileReader("in.txt"); 
         
         // Create the parser and pass the lexer
         Parser parser = new Parser(new Scanner(fileReader)); 
-
-        try {
-            // Parse the input and get the result
-            Object result = parser.parse().value;
-            System.out.println("Parsing result: \n" + result);
-        } catch (Exception e) {
-            System.err.println("Parsing failed: " + e.getMessage());
-            e.printStackTrace();
-        }
+        parser.parse();
+        //parser.debug_parse();  
 
         fileReader.close(); // Close the file after reading
     }
