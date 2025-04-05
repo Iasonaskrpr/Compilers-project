fe63e18ff5d60d3f5115e1f665ca3b082aa6e0fe
Iasonaskrpr <iaskarapro@gmail.com>
2025-04-05 15:27:47 +0300

    Need to resolve reduce/reduce for funcalll inside fundef

--- Part2/Main.java
+++ Part2/Main.java
@@ -6,19 +6,19 @@ public class Main {
         PrintStream originalOut = System.out;
         PrintStream outFile = new PrintStream(new FileOutputStream("Translated.ir"));
         System.setOut(outFile);
-        // Read input from the file
-        FileReader fileReader = new FileReader("in.txt"); 
         
         // Create the parser and pass the lexer
-        Parser parser = new Parser(new Scanner(fileReader)); 
+        Reader stdinReader = new InputStreamReader(System.in);
+        Parser parser = new Parser(new Scanner(stdinReader));
         parser.parse();
          
         outFile = new PrintStream(new FileOutputStream("Translated.java"));
         System.setOut(outFile);
-        FileReader fileReader2 = new FileReader("Translated.ir");
-        Parser2 parser2 = new Parser2(new Scanner(fileReader2));
+        FileReader fileReader = new FileReader("Translated.ir");
+        Parser2 parser2 = new Parser2(new Scanner(fileReader));
         parser2.parse();
         parser2.debug_parse(); 
         fileReader.close(); // Close the file after reading
+        stdinReader.close();
     }
 }
